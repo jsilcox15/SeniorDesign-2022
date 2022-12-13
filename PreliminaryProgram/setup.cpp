407b989b52127f6f3f3171dc2e70f9403edfdeb5
jsilcox15 <117590476+jsilcox15@users.noreply.github.com>
2022-12-12 20:34:57 -0500

    Update setup.cpp

--- PreliminaryProgram/setup.cpp
+++ PreliminaryProgram/setup.cpp
@@ -50,7 +50,101 @@ std::string hextobin(const std::string &s){
 
 
 
-int main(int argc, char* argv[])
+int partition(int arr[], int start, int end)
+
+{
+
+
+
+	int pivot = arr[start];
+
+
+
+	int count = 0;
+
+	for (int i = start + 1; i <= end; i++) {
+
+		if (arr[i] <= pivot)
+
+			count++;
+
+	}
+
+
+
+	int pivotIndex = start + count;
+
+	std::swap(arr[pivotIndex], arr[start]);
+
+
+
+	int i = start, j = end;
+
+
+
+	while (i < pivotIndex && j > pivotIndex) {
+
+
+
+		while (arr[i] <= pivot) {
+
+			i++;
+
+		}
+
+
+
+		while (arr[j] > pivot) {
+
+			j--;
+
+		}
+
+
+
+		if (i < pivotIndex && j > pivotIndex) {
+
+			std::swap(arr[i++], arr[j--]);
+
+		}
+
+	}
+
+
+
+	return pivotIndex;
+
+}
+
+
+
+void quickSort(int arr[], int start, int end)
+
+{
+
+
+
+	if (start >= end)
+
+		return;
+
+
+
+	int p = partition(arr, start, end);
+
+
+
+	quickSort(arr, start, p - 1);
+
+
+
+	quickSort(arr, p + 1, end);
+
+}
+
+
+
+void encrypt(int input)
 
 {
 
@@ -84,7 +178,7 @@ int main(int argc, char* argv[])
 
 
 
-    std::string plain = "93";
+    std::string plain = std::to_string(input);
 
     std::string cipher, recovered;
 
@@ -284,8 +378,42 @@ int main(int argc, char* argv[])
 
     }
 
+}
 
 
-    return 0;
+
+int main()
+
+{
+
+	int n = 10;
+
+	int arr[n];
+
+	for(int i = 0; i < n; i++){
+
+		arr[i] = (rand() % 100);
+
+	}
+
+
+
+	quickSort(arr, 0, n-1);
+
+
+
+	for (int i = 0; i < n; i++) {
+
+		std::cout << arr[i] << " ";
+
+	}
+
+	
+
+	encrypt(arr[n-1]);
+
+	
+
+	return 0;
 
 }
